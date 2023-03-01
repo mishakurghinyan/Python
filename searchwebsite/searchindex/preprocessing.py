e7af0ffcce1ddca4d9082c227812a21000ef1c5c
mishakurghinyan <misha.kurghinyan.g@tumo.org>
2023-03-01 16:08:58 +0400

    lesson2start

--- searchwebsite/searchindex/preprocessing.py
+++ searchwebsite/searchindex/preprocessing.py
@@ -1,20 +1,25 @@
 import string
+import re
+from nltk.stem import *
+
 # Preprocess text for searching
 
-
+stemmer = PorterStemmer()
 stop = open('searchindex/stopwords_en.txt').read().splitlines()
 
 
 def preprocess_text(text:str) -> list[str]:
     """Preprocess the text for optimal search performance"""
-    x = text.lower()
-    new = x.translate(str.maketrans('', '', string.punctuation))
-    newer = new.split(" ")
-    final = []
-    for i in newer:
+    lowered = text.lower()
+    punctRem = lowered.translate(str.maketrans('', '', string.punctuation))
+    splited = punctRem.split(" ")
+    stopRem = []
+    for i in splited:
         if i not in stop:
-            final.append(i)
-
-    # Currently no preprocessing implemented
-    return final
+            stopRem.append(i)
+            stemRem = []
+    for x in stopRem:
+        stemRem.append(stemmer.stem(x))
+        # Currently no preprocessing implemented
+    return stemRem
 
